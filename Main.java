3708fde334f257f2f02c652b0da1092657c7b3a7
BADRobin <olegbryl@gmail.com>
2018-06-18 20:47:18 +0300

    lec2_10

---
+++ Main.java
@@ -0,0 +1,15 @@
+package lec210;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Integer a = 1000;
+	    Integer b = 1000;
+	    Integer c = Integer.valueOf("1000");
+        System.out.println(a==b);
+        System.out.println(a==c);
+        System.out.println(b==c);
+
+
+    }
+}
