c6369e1c5c75cdbd6f8c3f9e7e1a442c4c97c59f
BADRobin <olegbryl@gmail.com>
2018-06-18 21:20:22 +0300

    lec2_10

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ package lec210;
 public class Main {
 
     public static void main(String[] args) {
-	   int x1 =10;
-	   int x2 =0;
-
+	  String st = "4.5";
+	  Double d = Double.valueOf(st);
+        System.out.println(d);
     }
 }
